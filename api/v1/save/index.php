71cddb8bcb5036a9c9c162afe32041e67653182d
hidao80 <hidao80@users.noreply.github.com>
2022-01-25 11:31:10 +0900

    fix: required for physical servers.

--- api/v1/save/index.php
+++ api/v1/save/index.php
@@ -1,27 +1,32 @@
 <?php
 declare(strict_types=1);
 
+header("Content-Type: application/json; charset=UTF-8");
+
 require_once(__DIR__ . '/../../../lib/global_variables.php');
 require_once(__DIR__ . '/../../../lib/functions.php');
 
 $json = file_get_contents("php://input");
-log_output(var_export($json, true));
+log_output('api/v1/save: ' . var_export($json, true));
 
-// Only accept requests from the same host
-if ($_SERVER['REMOTE_ADDR'] !== gethostbyname('localhost')) {
-    http_response_code(403);
-    echo json_encode([
-        "status" => "403",
-        "message" => 'Not the same host'
-    ]);
+$contents = json_decode($json, true);
+log_output('api/v1/save: ' . var_export($contents, true));
+
+$user_id = $contents['user_id'];
+log_output('api/v1/save: user_id = ' . $user_id);
+if (empty($user_id)) {
+    log_output('api/v1/save: user_id is empty');
+    http_response_code(400);
     return;
 }
 
-$contents = json_decode($json, true);
-log_output(var_export($contents, true));
-
-$user_id   = $contents['user_id'];
 $todo_list = array_filter(explode("\n", $contents['todo']));
+log_output('api/v1/save: todo_list = ' . var_export($todo_list, true));
+if (empty($todo_list)) {
+    log_output('api/v1/save: todo list is empty');
+    http_response_code(400);
+    return;
+}
 
 $DB->exec('BEGIN');
 
@@ -29,7 +34,7 @@ try {
     foreach ($todo_list as $value) {
         $sql    = "INSERT INTO tasks (user_id, task, status, created_at) VALUES (?, ?, 0, date('now', 'localtime'))";
         $params = [$user_id, trim($value)];
-        log_output(var_export([$sql, $params], true));
+        log_output('api/v1/save: ' . var_export([$sql, $params], true));
 
         $DB->prepare($sql)->execute($params);
     }
@@ -37,7 +42,7 @@ try {
     $DB->exec('COMMIT');
 } catch (Exception $ex) {
     $DB->exec('ROLLBACK');
-    log_output($ex->getMessage());
+    log_output('api/v1/save: ' . $ex->getMessage());
     http_response_code(500);
     echo json_encode([
         "status" => "500",
@@ -47,4 +52,4 @@ try {
 }
 
 http_response_code(200);
-echo '';
+echo '{}';
--- api/v1/update/index.php
+++ api/v1/update/index.php
@@ -1,27 +1,32 @@
 <?php
 declare(strict_types=1);
 
+header("Content-Type: application/json; charset=UTF-8");
+
 require_once(__DIR__ . '/../../../lib/global_variables.php');
 require_once(__DIR__ . '/../../../lib/functions.php');
 
 $json = file_get_contents("php://input");
-log_output(var_export($json, true));
+log_output('api/v1/update: json = ' . var_export($json, true));
 
-// Only accept requests from the same host
-if ($_SERVER['REMOTE_ADDR'] !== gethostbyname('localhost')) {
-    http_response_code(403);
-    echo json_encode([
-        "status" => "403",
-        "message" => 'Not the same host'
-    ]);
+$contents = json_decode($json, true);
+log_output('api/v1/update: contents = ' . var_export($contents, true));
+
+$user_id = $contents['user_id'];
+log_output('api/v1/update: user_id = ' . $user_id);
+if (empty($user_id)) {
+    log_output('api/v1/save: user_id is empty');
+    http_response_code(400);
     return;
 }
 
-$contents = json_decode($json, true);
-log_output(var_export($contents, true));
-
-$user_id = $contents['user_id'];
 $content = $contents['tasks']['content'];
+log_output('api/v1/save: tasks content = ' . var_export($content, true));
+if (empty($content)) {
+    log_output('api/v1/save: tasks content is empty');
+    http_response_code(400);
+    return;
+}
 
 $DB->exec('BEGIN');
 
@@ -29,7 +34,7 @@ try {
     foreach ($content as $task) {
         $sql    = "UPDATE tasks SET status = ?, comment = ? WHERE id = ?";
         $params = [$task['status'], $task['comment'], $task['id']];
-        log_output(var_export([$sql, $params], true));
+        log_output('api/v1/update: ' . var_export([$sql, $params], true));
         
         $DB->prepare($sql)->execute($params);
     }
@@ -37,7 +42,7 @@ try {
     $DB->exec('COMMIT');
 } catch (Exception $ex) {
     $DB->exec('ROLLBACK');
-    log_output($ex->getMessage());
+    log_output('api/v1/update: ' . $ex->getMessage());
     http_response_code(500);
     echo json_encode([
         "status" => "500",
@@ -47,4 +52,4 @@ try {
 }
 
 http_response_code(200);
-echo '';
+echo '{}';
